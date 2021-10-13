ffbb55e337273056fda5517420011005a5756cc1
Grigorii Ozhegov <ozhegov@yandex-team.ru>
2021-10-13 06:18:51 +0300

    default lang, 2 systemd

--- presentation/03-run-systemd/shbr-echo/build/install.sh
+++ presentation/03-run-systemd/shbr-echo/build/install.sh
@@ -6,4 +6,5 @@ mkdir -p "$DESTDIR/etc/systemd/system/"
 
 cp libs/java-0.0.1-SNAPSHOT.jar "$DESTDIR/usr/bin/shbr-echo.jar"
 cp main.py "$DESTDIR/usr/bin/shbr-echo.py"
-cp ../shbr-echo.service "$DESTDIR/etc/systemd/system/"
+cp ../shbr-echo-java.service "$DESTDIR/etc/systemd/system/"
+cp ../shbr-echo-python.service "$DESTDIR/etc/systemd/system/"
