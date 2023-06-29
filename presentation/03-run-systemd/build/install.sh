66f89edd15ab09504e4cb3e74d119389963637df
Grigorii Ozhegov <ozhegov@yandex-team.ru>
2023-06-29 13:23:42 +0300

    fix makefiles

--- presentation/03-run-systemd/build/install.sh
+++ presentation/03-run-systemd/build/install.sh
@@ -5,7 +5,7 @@ mkdir -p "$DESTDIR/usr/bin/"
 mkdir -p "$DESTDIR/etc/systemd/system/"
 mkdir -p "$DESTDIR/etc/shbr-echo/cpp"
 
-cp ../cpp/build_release/shbr_devops_cpp "$DESTDIR/usr/bin/shbr-echo"
+cp ../cpp/build_release/shbr_devops_cpp "$DESTDIR/usr/bin/shbr-echo-cpp"
 cp -r ../cpp/configs "$DESTDIR/etc/shbr-echo/cpp"
 
 cp build-java/libs/java-0.0.1-SNAPSHOT.jar "$DESTDIR/usr/bin/shbr-echo.jar"
